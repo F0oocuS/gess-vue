07d53a24486217eb3fb734448f238c37b38c7774
F0oocus <ihoryaremko@gmail.com>
2017-10-17 13:13:51 +0300

    simple structure

---
+++ src/components/general/BlockTitle.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="block-title">
+        <h3 class="title">Test block title {{ blockTitle }}</h3>
+        <div class="separate">
+            <div class="green"></div>
+            <div class="lemon"></div>
+            <div class="white"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['blockTitle'],
+        data() {
+            return {
+                blockTitle: blockTitle
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .title {
+        margin-bottom: 15px;
+        font-family: 'Roboto Slab', Serif;
+        text-align: center;
+        text-transform: uppercase;
+        font-size: 24px;
+        font-weight: 700;
+        letter-spacing: 2px;
+    }
+    .separate {
+        .green {
+            margin: auto;
+            height: 4px;
+            width: 120px;
+        }
+        .lemon {
+            margin: auto;
+            height: 2px;
+            width: 200px;
+        }
+        .white, .grey {
+            margin: auto;
+            height: 1px;
+            width: 300px;
+        }
+    }
+</style>
---
+++ src/components/general/UnorderedList.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul class="list">
+        <li class="list__item" v-for="item in list">{{ item }}</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                list: ['cat', 'dog', 'bird', 'pig']
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .list {
+        list-style: none;
+        padding-left: 40px;
+    }
+    .list__item {
+        position: relative;
+        &:before {
+            content: '';
+            position: absolute;
+            left: -30px;
+            top: calc(50% - 5px);
+            width: 10px;
+            height: 10px;
+            border-right: 5px;
+            background-color: #64ff4d;
+        }
+    }
+</style>
